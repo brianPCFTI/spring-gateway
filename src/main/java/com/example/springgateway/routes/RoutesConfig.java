e63ebe5eeee34728e77bc50a6bd4f81c6cd00dba
Brian Mora <brian.mora@pfcti.com>
2023-03-30 18:54:54 -0600

    upd

--- src/main/java/com/example/springgateway/routes/RoutesConfig.java
+++ src/main/java/com/example/springgateway/routes/RoutesConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class RoutesConfig {
@@ -21,6 +22,16 @@ public class RoutesConfig {
                                         f -> f.addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY_REQUEST")
                                                 .addResponseHeader("X-GATEWAY-RESPONSE-HEADER", "GATEWAY_RESPONSE"))
                                 .uri(clientesUri))
+                .route("all_clientes",
+                        r -> r.path("/promerica/v1/api/cliente/all")
+                                .and().method(HttpMethod.GET)
+                                .filters(
+                                        f -> f.rewritePath("/promerica/v1/api/cliente/all", "/v1/api/cliente/all")
+                                                .addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY_REQUEST")
+                                                .addResponseHeader("X-GATEWAY-RESPONSE-HEADER", "GATEWAY_RESPONSEDEALBERTOSALAZAR"))
+                                .uri(clientesUri))
                 .build();
     }
 }
+
+
